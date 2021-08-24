b9806d0db7214313c9f5164680904888cda483c4
Gautham Yadav <82380930+Gauthamyadav777@users.noreply.github.com>
2021-08-24 13:08:54 +1000

    Adding Files

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors"); // cors -Cross-Origin Resource Sharing
+const knex = require("knex"); // library for connecting SQL database
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  version: "7.2",
+  connection: {
+    host= '127.0.0.1',
+    user= 'gautham',
+    password='',
+    database='Food-Detection'
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("this is working");
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleImageUrl(req, res);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`server is running on port ${process.env.PORT}`)
+);
